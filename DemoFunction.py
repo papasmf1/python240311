3c4a7f6c387aca857485891fb856cc9f32cda711
papasmf1 <papasmf1@gmail.com>
2024-03-11 14:50:37 +0900

    함수 연습2

--- DemoFunction.py
+++ DemoFunction.py
@@ -8,3 +8,29 @@ def setValue(newValue):
 #호출 
 print(setValue(5))
 
+#튜플로 리턴 
+def swap(x,y):
+    return y,x 
+#호출
+print(swap(3,4))
+#지역변수와 전역변수
+x = 10 
+def func(a):
+    return a+x 
+#호출
+print(func(1))
+
+def func2(a):
+    x = 5 
+    return a+x 
+#호출
+print(func2(1))
+
+#기본값 셋팅
+def times(a=10, b=20):
+    return a*b 
+
+print(times())
+print(times(5))
+print(times(5,6))
+
